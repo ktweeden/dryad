01df8722108e238cbec0f9da101258693f18d70b
Kate Preston <ktweeden@googlemail.com>
2018-05-12 22:55:58 +0100

    index, insert and destroy routes for story sections

--- server/controllers/StorySectionController.js
+++ server/controllers/StorySectionController.js
@@ -1,6 +1,45 @@
 const express = require('express')
 const storySectionRouter = new express.Router()
+const StorySection = require('../schemas/StorySection.js')
 
+storySectionRouter.get('/', function (req, res) {
+    StorySection.find(function (err, sections) {
+        if (err) {
+            console.error(err);
+            res.status(500);
+            res.send();
+            return;
+        };
+        res.json(sections)
+    })
+})
 
+storySectionRouter.post('/', function (req, res) {
+    const newStorySection = new StorySection(req.body)
+    newStorySection.save(function (err, storySection) {
+        if (err) {
+            console.error(err);
+            res.status(500);
+            res.send();
+            return;
+        };
+        res.status(201);
+        res.json(storySection);
+    });
+});
+
+storySectionRouter.delete('/', function (req, res) {
+    StorySection.deleteMany(function (err) {
+        if (err) {
+            console.error(err);
+            res.status(500);
+            res.send();
+            return;
+        };
+        console.log('Everything has gone');
+        res.status(204);
+        res.send();
+    })
+})
 
 module.exports = storySectionRouter
